54020ac24ec331d408ea93d8617e99a6b5384e9e
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-03 22:22:59 -0300

    yet looking into functions

--- 003/module.ex
+++ 003/module.ex
@@ -2,6 +2,15 @@ defmodule CreatingModule.SaySomething do
   def say_something
      "Elixir praticing!"
   end
+
+  @doc """
+    we can use anonymous functions here, like in javascript,
+    and we will use it, much similar to arrow functions, take a look:
+    sum = fn (a,b ) -> a + b end
+
+    to execute it, we need to run: sum.(1,2) (using the dot notation)
+  """
+
 end
 
 # this will be accessed as CreatingModule.SaySomething.say_something
